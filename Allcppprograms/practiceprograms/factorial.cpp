4877391eff66e809849b587dd4d2c73c6798b1c9
mahivellanki <mahivellanki557@gmail.com>
2025-10-20 15:16:28 +0000

    Add GitHub Actions CI workflow

--- Allcppprograms/practiceprograms/factorial.cpp
+++ Allcppprograms/practiceprograms/factorial.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"maheshenter number"<<endl;
+    cout<<"mahesh enter number: ";
     cin>>n;
     int results = 1;
-    for(int i=1;i<=n;i++){
+    if(n == 1 || n == 0)
+        cout<<"Factorial of "<<n<<" is: 1"<<endl;
+        return 0;
+    for(int i=2;i<=n;i++){
         results=results*i;
         
     }
-    cout<<"Factoroal of "<<n<<"is:"<<results<<endl;
+    cout<<"Factorial of "<<n<<" is:"<<results<<endl;
 }
