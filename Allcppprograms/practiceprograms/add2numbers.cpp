e2ef0e0ce0bac7f7f55296ea5557ee6116540180
mahivellanki <mahivellanki557@gmail.com>
2025-10-25 15:29:33 +0000

    Test Githubactions.

--- Allcppprograms/practiceprograms/add2numbers.cpp
+++ Allcppprograms/practiceprograms/add2numbers.cpp
@@ -6,6 +6,11 @@ int main(){
     cout<<"Enter two numbers to add: ";
     cin>>a>>b;
     cout<<"Sum is: "<<a+b<<endl;
+    cout<<"Enter first number to add: ";
+    cin>>a;
+    cout<<"Enter second number to add: ";
+    cin>>b;
+    cout<<"Sum of two numbers: "<<a+b<<endl;
     return 0;
 
 }
