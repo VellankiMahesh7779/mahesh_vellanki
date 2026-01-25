8415dbdac62245cef3e08d733291a24d4d664761
mahivellanki <mahivellanki557@gmail.com>
2026-01-25 16:45:12 +0000

    polymorphism.

---
+++ Allcppprograms/functions/poly.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class addition {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+};
+
+int main() {
+    Math m;
+    cout << m.add(2, 3) << endl;
+    cout << m.add(2.5, 3.5) << endl;
+    return 0;
+}
