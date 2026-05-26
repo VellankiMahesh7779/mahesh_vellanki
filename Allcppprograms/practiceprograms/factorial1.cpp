7a151cd292c0a61e57e2ad7563e5b6f8cbd79db6
mahivellanki <mahivellanki557@gmail.com>
2026-05-26 11:56:23 +0000

    Factorial CPP

---
+++ Allcppprograms/practiceprograms/factorial1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+long long fact(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    return n * fact(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Not defined";
+    else
+        cout << "Factorial is: " << fact(n);
+
+    return 0;
+}
